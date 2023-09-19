9013edb3b9dcdb48ba053aaf1eb7f0f0bdb29beb
Andrew <andrewstaveley14@gmail.com>
2023-09-19 17:45:16 -0400

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,8 @@
+const divide = function() {
+    let answer = (2000/100);
+    return answer;
+}
+
+const square = parameter => (parameter * parameter);
+
+const add = (parameter, parameter2) => (parameter + parameter2);
